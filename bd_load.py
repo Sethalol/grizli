bdf3e80fd550c8d687839a796bae084f8223ca60
sethalol <239008923+Sethalol@users.noreply.github.com>
2026-08-09 14:31:46 +0400

    zalupaa

---
+++ bd_load.py
@@ -0,0 +1,39 @@
+import json
+import psycopg2
+from psycopg2.extras import execute_values
+
+rows=[]
+
+
+conn = psycopg2.connect(
+    host = 'localhost',
+    database = 'meduza',
+    user = 'admin',
+    password = '1111'
+)
+
+cursor = conn.cursor()
+
+with open ('meduza_news.json', encoding='utf-8') as f:
+    articles = json.load(f)
+
+for a in articles:
+    rows.append((
+        a.get('title'),
+        a.get('url'),
+        json.dumps(a, ensure_ascii=False)
+    ))
+
+execute_values(cursor, """
+    INSERT INTO articles (title, url, raw_json)
+    VALUES %s
+    ON CONFLICT (url) DO UPDATE SET
+        title = EXCLUDED.title,
+        raw_json = EXCLUDED.raw_json
+""", rows)
+
+conn.commit()
+cursor.close()
+conn.close()
+
+print(f'Загруэено статей {len(rows)}')
--- parcer.py
+++ parcer.py
@@ -1,26 +1,19 @@
-import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 import json
 
 
-service = Service(GeckoDriverManager().install())
-driver = webdriver.Firefox(service=service)
-
-
 def parce_meduza():
     options = Options()
-    options.add_argument("--headless")  # Фоновый режим
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument("--headless")
 
+    service = Service(GeckoDriverManager().install())
+    driver = webdriver.Firefox(service=service, options=options)
+    
     try:
         print('Окрываем медузу')
         driver.get('https://meduza.io/')
